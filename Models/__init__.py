90285284ddc7013b0d79451ff48c356d24e08fa6
Yann Pequignot <yann.pequignot@gmail.com>
2021-01-27 12:13:47 -0500

    update import

--- Models/__init__.py
+++ Models/__init__.py
@@ -1,6 +1,6 @@
 from .GenerativeNetwork import BigGenerator
 from .MeanField import MeanFieldVariationalDistribution
 from .McDropOut import MC_Dropout_Model
-
+from .VI import MFVI, HyVI
 from .predictiveMLP import get_mlp
 from .GaussianProcess import GaussianProcess
