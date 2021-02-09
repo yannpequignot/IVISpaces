f1dc955a1d513b1b37a77e3f8cad455665097f67
Yann Pequignot <yann.pequignot@gmail.com>
2021-02-09 15:36:59 -0500

    cleaning

--- Inference/__init__.py
+++ Inference/__init__.py
@@ -1,3 +1,4 @@
 from .Ensemble import ensemble, ensemble_train
 from .VI import NN_train, FuNN_train, BBB_train
 from .MCdropout import MC_Dropout, MCdo_train
+from .HMC import hamiltonian_monte_carlo_da, AdamGradientDescent
